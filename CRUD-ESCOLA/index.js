85639f5f6992e541d2e7fe0c069e464c1397c4f9
KaioSantoss <kaiodaniel905@gmail.com>
2025-09-21 13:10:19 -0300

    feat: configuração do index parte alunos

--- CRUD-ESCOLA/index.js
+++ CRUD-ESCOLA/index.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-// TODO: Membro 1 - Importar e mapear rota de alunos
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-// TODO: Membro 2 - Importar e mapear rota de professores
+// Rotas de alunos - Membro 1: Kaio Daniel Almeida Santos
+const alunosRouter = require('./Routes/alunos');
+app.use('/alunos', alunosRouter);
+
+
+// Rotas de Professores - Membro 2: Jamylle Cesario Soares Silva
+
 
 app.listen(3000, () => {
 console.log('Server is running on http://localhost:3000')
